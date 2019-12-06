590f7dabe61a5b346e2aa79038c850b6aa961618
Fili Santillán <fili06604@gmail.com>
2019-12-06 04:35:10 -0600

    🎉 Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { ListOfCategories } from "./components/ListOfCategories";
+import { PhotoCard } from "./components/PhotoCard";
+
+import { GlobalStyle } from "./GlobalStyles";
+
+export const App = () => (
+  <React.Fragment>
+    <GlobalStyle />
+    <ListOfCategories />
+    <PhotoCard />
+  </React.Fragment>
+);
