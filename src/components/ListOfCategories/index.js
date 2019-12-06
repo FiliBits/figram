2419852fa7ad467c88c161c9dd38bc6be4125191
Fili Santillán <fili06604@gmail.com>
2019-12-06 15:02:20 -0600

    💄 ✨ Add request categories, and hook to scroll

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Category } from "../Category";
 
 import { List, Item } from "./styles";
-import { categories } from "../../../api/db.json";
 
 export const ListOfCategories = () => {
-  return (
-    <List>
-      {
-        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
-      }
+  const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
+
+  useEffect(function() {
+    window.fetch("https://figram-server.now.sh/categories")
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, []);
+
+  useEffect(function() {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    }
+
+    document.addEventListener("scroll", onScroll);
+
+    return () => document.removeEventListener("scroll", onScroll);
+  }, [showFixed]);
+
+  const renderList = (fixed) => (
+    <List className={fixed ? "fixed" : ""}>
+      {categories.map(category => ( 
+        <Item key={category.id}>
+          <Category {...category} />
+        </Item>
+      ))}
     </List>
+  );
+
+  return (
+    <React.Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </React.Fragment>
   )
 }
+
