e634e2a60788c2bbb9b8408e4ad11f5a97cf54d1
Fili Santillán <fili06604@gmail.com>
2019-12-13 01:52:25 -0600

    ✨ 💄 Create custom hooks: useLocalStorage and useNearScreen

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,29 +1,22 @@
 import React, { useEffect, useRef, useState } from "react";
 
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { useNearScreen } from "../../hooks/useNearScreen";
+
 import { Article, ImgWrapper, Img, Button } from "./styles";
-import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 
 const DEFAULT_IMAGE =
   "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null);
-  const [show, setShow] = useState(false);
+  const [show, element] = useNearScreen();
 
-  useEffect(
-    function() {
-      const observer = new window.IntersectionObserver(function(entries) {
-        const { isIntersecting } = entries[0];
+  const key = `like-${id}`;
 
-        if (isIntersecting) {
-          setShow(true);
-          observer.disconnect();
-        }
-      });
-      observer.observe(element.current);
-    },
-    [element]
-  );
+  const [liked, setLiked] = useLocalStorage(key, false);
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder;
 
   return (
     <Article ref={element}>
@@ -35,8 +28,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button>
-            <MdFavoriteBorder size="25px" /> {likes} likes!
+          <Button onClick={() => setLiked(!liked)}>
+            <Icon size="25px" /> {likes} likes!
           </Button>
         </React.Fragment>
       )}
--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -6,7 +6,7 @@ export const Article = styled.article`
 `;
 
 export const ImgWrapper = styled.div`
-  border-radius: 10px;
+  /* border-radius: 10px; */
   display: block;
   height: 0;
   overflow: hidden;
