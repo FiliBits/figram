bff73dcf0c0bbafbdaa26df16d843bd13d04781b
Fili Santillán <fili06604@gmail.com>
2019-12-26 21:22:39 -0600

    ✨ 💄 NavBar added

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import { GlobalStyle } from "./styles/GlobalStyles";
 
 import { Logo } from "./components/Logo";
 
+import { NavBar } from "./components/NavBar/";
+
 import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
 
 import { Router } from "@reach/router";
 
 export const App = () => {
-
   return (
     <React.Fragment>
       <GlobalStyle />
       <Logo />
       <Router>
-          <Home path="/"/>
-          <Home path="/pet/:id" />
-          <Detail path="/detail/:detailId"/>
-        </Router>
+        <Home path="/" />
+        <Home path="/pet/:id" />
+        <Detail path="/detail/:detailId" />
+      </Router>
+      <NavBar />
     </React.Fragment>
   );
 };
