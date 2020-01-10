8335412df316b990169f1013fc4c54625f6797aa
Fili Santillán <fili06604@gmail.com>
2020-01-09 22:09:24 -0600

    ✨ Private route added

--- src/App.js
+++ src/App.js
@@ -8,8 +8,15 @@ import { NavBar } from "./components/NavBar/";
 
 import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
+import { Favs } from "./pages/Favs";
+import { User } from "./pages/User";
+import { NotRegisteredUser } from "./pages/NotRegisteredUser";
 
 import { Router } from "@reach/router";
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false });
+};
 
 export const App = () => {
   return (
@@ -21,6 +28,23 @@ export const App = () => {
         <Home path="/pet/:id" />
         <Detail path="/detail/:detailId" />
       </Router>
+
+      <UserLogged>
+        {({ isAuth }) =>
+          isAuth ? (
+            <Router>
+              <Favs path="/favs" />
+              <User path="/user" />
+            </Router>
+          ) : (
+            <Router>
+              <NotRegisteredUser path="/favs" />
+              <NotRegisteredUser path="/user" />
+            </Router>
+          )
+        }
+      </UserLogged>
+
       <NavBar />
     </React.Fragment>
   );
